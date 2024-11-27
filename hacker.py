8036b3a301b03837d10106b3ba26c8aef89525aa
nik-kurdupov <kurdupov.m@academy.com.ua>
2024-11-27 16:56:09 +0200

    Create hacker.py

---
+++ hacker.py
@@ -0,0 +1,14 @@
+login = input("enter your email")
+password = input("enter your password")
+
+
+right_login = "user228"
+right_password = "52"
+x = 0
+y = 0
+for i in range(10):
+    for t in range(10):
+ pas = i*10 + t
+ print(pas)
+  if pas == right_password
+  print("access granted")
